908c605d51bdfc164eef8a34259b9fc4b04e1ff6
mnordby-splunk <mnordby@splunk.com>
2025-09-08 10:52:54 -0600

    chore: update reference to outdated package

--- elasticsearch_connector.py
+++ elasticsearch_connector.py
@@ -14,9 +14,9 @@
 # and limitations under the License.
 """Code that implements calls made to the elasticsearch systems device"""
 
-import imp
 import json
 import sys
+import types
 import urllib.parse as urllib
 
 import phantom.app as phantom
@@ -404,7 +404,7 @@ class ElasticsearchConnector(BaseConnector):
                 if parser:
                     parser_name = config["ingest_parser__filename"]
                     self.save_progress(f"Using specified parser: {parser_name}")
-                    ingest_parser = imp.new_module("custom_parser")
+                    ingest_parser = types.ModuleType("custom_parser")
                     try:
                         sys.stdout = debug_out
                         exec(parser, ingest_parser.__dict__)
